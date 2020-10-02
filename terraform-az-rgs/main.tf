e0efa027cedbdfc89405035d76362bb2741684a1
johninge@varleg.no <john@varleg.no>
2020-10-02 08:37:51 +0200

    init

---
+++ terraform-az-rgs/main.tf
@@ -0,0 +1,21 @@
+
+resource "azurerm_resource_group" "rg1" {
+  name = "rg1"
+  location = "northeurope"
+}
+resource "azurerm_resource_group" "rg2" {
+  name = "rg2"
+  location = "northeurope"
+}
+resource "azurerm_resource_group" "rg3" {
+  name = "rg3"
+  location = "northeurope"
+}
+resource "azurerm_resource_group" "rg4" {
+  name = "rg4"
+  location = "northeurope"
+}
+resource "azurerm_resource_group" "rg5" {
+  name = "rg5"
+  location = "northeurope"
+}
